c7ec519a05e092f5a7e3f1b739aa791f02c3136e
TRANDOANCHITHANH <tranchithanh0404@gmail.com>
2024-11-01 01:27:47 +0700

    Add Coupon

---
+++ WebShopping/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShopping.Models;
+using WebShopping.Repository;
+
+namespace WebShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Coupon")]
+    public class CouponController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CouponController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var couponList = await _dataContext.Coupons.ToListAsync();
+            ViewBag.CouponList = couponList;
+            return View();
+        }
+        [HttpGet]
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Route("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CouponModel coupon)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.Add(coupon);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Thêm coupon thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Error";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n ", errors);
+                return BadRequest(errorMessage);
+            }
+            return View();
+        }
+    }
+}
---
+++ WebShopping/Models/CouponModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopping.Models
+{
+	public class CouponModel
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Yêu cầu không được để trống")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Yêu cầu không được để trống")]
+		public string Description { get; set; }
+		[Required(ErrorMessage = "Yêu cầu không được để trống")]
+		public int Quantity { get; set; }
+		public DateTime DateStart { get; set; }
+		public DateTime DateExpired { get; set; }
+		public int Status { get; set; }
+	}
+}
